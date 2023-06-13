516469ca3749b817caa3798fefa05810e050f4f2
Ashley Blurton <4752876+akblurton@users.noreply.github.com>
2023-06-13 10:01:52 +0100

    fix: 🐛 do not use custom font due to text mangling in react-pdf

--- src/Document/font.ts
+++ src/Document/font.ts
@@ -1,21 +1,6 @@
-import { Font } from "@react-pdf/renderer";
-
-import Regular from "@fontsource/lato/files/lato-latin-400-normal.woff";
-import ItalicRegular from "@fontsource/lato/files/lato-latin-400-italic.woff";
-import Bold from "@fontsource/lato/files/lato-latin-700-normal.woff";
-import ItalicBold from "@fontsource/lato/files/lato-latin-700-italic.woff";
-
-const FONT_FAMILY = "Lato";
-Font.register({
-  family: FONT_FAMILY,
-  fonts: [
-    { src: Regular },
-    { fontStyle: "italic", src: ItalicRegular },
-    { fontWeight: "bold", src: Bold },
-    { fontWeight: "bold", fontStyle: "italic", src: ItalicBold },
-  ],
-});
-
-Font.registerHyphenationCallback((word) => [word]);
+const FONT_FAMILY = "Helvetica";
+export const BOLD = "Helvetica-Bold";
+export const BOLD_ITALIC = "Helvetica-BoldOblique";
+export const ITALIC = "Helvetica-Oblique";
 
 export default FONT_FAMILY;

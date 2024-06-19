f15abb51da88739fb451f9736cec752f5c5e7901
Ashley Blurton <4752876+akblurton@users.noreply.github.com>
2024-06-19 20:51:44 +0100

    feat: ✨ small tweak to interests section

--- src/Document/Interests.tsx
+++ src/Document/Interests.tsx
@@ -1,52 +1,20 @@
-import { StyleSheet, View, Text } from "@react-pdf/renderer";
-import { Style } from "@react-pdf/types";
-
-import Image from "./Image";
 import { Heading, Paragraph } from "./Typography";
-
-import skills from "../data/skills";
-
-const styles = StyleSheet.create({
-  skills: {
-    flexDirection: "row",
-    flexWrap: "wrap",
-    justifyContent: "center",
-    gap: "0.2cm",
-    marginTop: "0.2cm",
-    marginBottom: "0.5cm",
-  },
-  skill: {
-    padding: "0.2cm",
-    backgroundColor: "#F2F2F2",
-    borderRadius: "3px",
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "center",
-    fontSize: "8pt",
-    flexShrink: 1,
-    flexGrow: 1,
-  },
-  skillLogo: {
-    width: "0.5cm",
-    height: "0.5cm",
-    marginRight: "0.2cm",
-    objectFit: "contain",
-  },
-});
 
 const Interests: React.FC = () => {
   return (
     <>
       <Heading>Personal Interests</Heading>
       <Paragraph>
-        I love trying to solve all sorts of problems, even if mundane with code.
-        This CV is written with React! Which helps me make better and more
-        interesting formatting decisions and quicker updates
+        I love trying to solve all sorts of problems, even if they're mundane,
+        with code. This CV is written with React! Which helps me make better and
+        more interesting formatting decisions and quicker updates.
       </Paragraph>
       <Paragraph>
         My love of code based solutions has led me down the home automation
         black hole. Planning, testing, and experimenting with small changes in
-        how my day to day life is automated is endlessly rewarding!
+        how my day to day life is automated is endlessly rewarding! From
+        automated trash day reminders, to motion sensitive lighting, and onward
+        to automated blinds based on the position of the sun.
       </Paragraph>
       <Paragraph>
         Picked up an obsessive coffee hobby during 2020 lockdowns and since
